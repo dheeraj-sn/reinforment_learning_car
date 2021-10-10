5735640d94dfe31ef0c715ff40feb12a9fce36f5
Dheeraj Singh <dheeraj.student2@gmail.com>
2021-10-10 15:36:35 +0800

    Refactored

--- car_environment.py
+++ car_environment.py
@@ -12,6 +12,13 @@ from pymunk.pygame_util import DrawOptions as draw
 # PyGame init
 width = 1000
 height = 700
+left = 0
+right = width -1
+top = 0
+bottom = height - 1
+
+
+FPS = 10
 pygame.init()
 screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
@@ -23,6 +30,93 @@ screen.set_alpha(None)
 show_sensors = True
 draw_screen = True
 
+# Colors
+color_red = pygame.Color(255, 0, 0)
+color_green = pygame.Color(0, 255, 0)
+color_blue = pygame.Color(0, 0, 255)
+color_grey = pygame.Color(128, 128, 128)
+color_orange = pygame.Color(255, 165, 0)
+color_black = pygame.Color(255, 255, 255)
+color_white = pygame.Color(0,0,0)
+
+def convert_coordinates_pymunk_to_pygame(point):
+    return point[0], height - point[1]
+    # pymunk top,left = 0,0
+    # pygame bottom,left = 0, 0
+    # x is the same ie width
+    # y changes, i.e. height
+
+
+class Wall():
+    def __init__(self,p1,p2):
+        self.body = pymunk.Body(body_type=pymunk.Body.STATIC)
+        self.shape = pymunk.Segment(body=self.body, a=p1, b=p2, radius=50)
+        self.shape.elasticity = 1
+        self.shape.collision_type = 1
+        self.shape.friction = 1
+        self.shape.color = THECOLORS['red']
+        self.shape.group = 1
+        #space.add(self.body,self.shape)
+    
+    def draw(self):
+        pygame.draw.line(surface=screen, color=self.shape.color,\
+                start_pos=convert_coordinates_pymunk_to_pygame(self.shape.a), \
+                end_pos=convert_coordinates_pymunk_to_pygame(self.shape.b))
+
+class Obstacle():
+    def __init__(self, x, y, r):
+        self.body = pymunk.Body(body_type=pymunk.Body.STATIC)
+        self.shape = pymunk.Circle(body=self.body, radius=r)
+        self.body.position = x, y
+        self.shape.density = 1
+        self.shape.elasticity = 1.0
+        self.shape.color = color_blue
+        #self.shape.collision_type = 1
+        #space.add(self.body, self.shape)
+    
+    def draw(self):
+        pygame.draw.circle(surface=screen, color=self.shape.color, \
+            center=convert_coordinates_pymunk_to_pygame(self.body.position), \
+            radius=self.shape.radius)
+    
+    def reset(self, space, arbiter, data):
+        self.body.position = middlex, middley
+        self.body.velocity = 400, -300
+        return
+
+class Cat():
+    def __init__(self):
+        inertia = pymunk.moment_for_circle(1, 0, 14, (0, 0))
+        self.body = pymunk.Body(1, inertia)
+        self.body.position = 50, height - 100
+        self.shape = pymunk.Circle(self.body, 30)
+        self.shape.color = color_orange
+        self.shape.elasticity = 1
+        self.shape.angle = 0.5
+        direction = Vec2d(1, 0).rotated(self.body.angle)
+    
+    def draw(self):
+        pygame.draw.circle(surface=screen, color=self.shape.color, \
+            center=convert_coordinates_pymunk_to_pygame(self.body.position), \
+            radius=self.shape.radius)
+
+class Car():
+    def __init__(self, x, y, r):
+        inertia = pymunk.moment_for_circle(1, 0, 14, (0, 0))
+        self.body = pymunk.Body(1, inertia)
+        self.body.position = x, y
+        self.shape = pymunk.Circle(self.body, 25)
+        self.shape.color = color_green
+        self.shape.elasticity = 1.0
+        self.body.angle = r
+        driving_direction = Vec2d(1, 0).rotated(self.body.angle)
+        self.body.apply_force_at_local_point(driving_direction)
+    
+    def draw(self):
+        pygame.draw.circle(surface=screen, color=self.shape.color, \
+            center=convert_coordinates_pymunk_to_pygame(self.body.position), \
+            radius=self.shape.radius)
+
 class GameState:
     def __init__(self):
         # Global-ish.
@@ -32,46 +126,46 @@ class GameState:
         self.space = pymunk.Space()
         self.space.gravity = pymunk.Vec2d(0., 0.)
 
-        # Create the car.
-        self.create_car(100, 100, 0.5)
-
         # Record steps.
         self.num_steps = 0
 
         # Create walls.
-        static = [
-            pymunk.Segment(
-                self.space.static_body,
-                (0, 1), (0, height), 1),
-            pymunk.Segment(
-                self.space.static_body,
-                (1, height), (width, height), 1),
-            pymunk.Segment(
-                self.space.static_body,
-                (width-1, height), (width-1, 1), 1),
-            pymunk.Segment(
-                self.space.static_body,
-                (1, 1), (width, 1), 1)
-        ]
-        for s in static:
-            s.friction = 1.
-            s.group = 1
-            s.collision_type = 1
-            s.color = THECOLORS['red']
-        for b in static:
-            self.space.add(b)
-        #self.space.add(static)
-
+        
+        wall_left = Wall([left, top], [left, bottom])
+        wall_right = Wall([right, top], [right, bottom])
+        wall_top = Wall([left, top], [right, top])
+        wall_bottom = Wall([left, bottom], [right, bottom])
+        self.walls = [wall_left, wall_right, wall_top, wall_bottom]
+        for wall in self.walls:
+            self.space.add(wall.body, wall.shape)
+    
         # Create some obstacles, semi-randomly.
         # We'll create three and they'll move around to prevent over-fitting.
         self.obstacles = []
-        self.obstacles.append(self.create_obstacle(200, 350, 100))
-        self.obstacles.append(self.create_obstacle(700, 200, 125))
-        self.obstacles.append(self.create_obstacle(600, 600, 35))
+        self.obstacles.append(Obstacle(200, 350, 100))
+        self.obstacles.append(Obstacle(700, 200, 125))
+        self.obstacles.append(Obstacle(600, 600, 35))
 
         # Create a cat.
-        self.create_cat()
+        self.cat = Cat()
+        self.cat_body = self.cat.body
+        self.space.add(self.cat.body, self.cat.shape)
+        #self.create_cat()
 
+        # Create car
+        self.car = Car(100, 100, 0.5)
+        self.car_body = self.car.body
+        self.space.add(self.car.body, self.car.shape)
+
+    def draw_all(self):
+        for wall in self.walls:
+            wall.draw()
+        for obstacle in self.obstacles:
+            obstacle.draw()
+        self.cat.draw()
+        self.car.draw()
+
+    """
     def create_obstacle(self, x, y, r):
         c_body = pymunk.Body(body_type=pymunk.Body.STATIC)
         c_shape = pymunk.Circle(c_body, r)
@@ -101,9 +195,9 @@ class GameState:
         self.car_shape.elasticity = 1.0
         self.car_body.angle = r
         driving_direction = Vec2d(1, 0).rotated(self.car_body.angle)
-        self.car_body.apply_force_at_local_point(driving_direction)
+        self.car_body.apply_impulse_at_local_point(driving_direction)
         self.space.add(self.car_body, self.car_shape)
-
+    """
     def frame_step(self, action):
         if action == 0:  # Turn left.
             self.car_body.angle -= .2
@@ -122,13 +216,18 @@ class GameState:
         self.car_body.velocity = 100 * driving_direction
 
         # Update the screen and stuff.
-        screen.fill(THECOLORS["black"])
-        draw(screen)
-        self.space.step(1./10)
-        if draw_screen:
-            pygame.display.flip()
-        clock.tick()
+        screen.fill(color_white)
 
+        #draw(screen)
+        self.draw_all()
+    
+        if draw_screen:
+            #pygame.display.update()
+            pygame.display.flip()
+        clock.tick(FPS)
+        self.space.step(1./FPS)
+        
+        
         # Get the current location and the readings there.
         x, y = self.car_body.position
         readings = self.get_sonar_readings(x, y, self.car_body.angle)
@@ -147,6 +246,7 @@ class GameState:
         self.num_steps += 1
 
         return reward, state
+        
 
     def move_obstacles(self):
         # Randomly move obstacles around.
@@ -178,11 +278,12 @@ class GameState:
             for i in range(10):
                 self.car_body.angle += .2  # Turn a little.
                 screen.fill(THECOLORS["grey7"])  # Red is scary!
-                draw(screen)
-                self.space.step(1./10)
+                #draw(screen)
+                self.draw_all()
+                self.space.step(1./FPS)
                 if draw_screen:
                     pygame.display.flip()
-                clock.tick()
+                clock.tick(FPS)
 
     def sum_readings(self, readings):
         """Sum the number of non-zero readings."""
@@ -272,6 +373,14 @@ class GameState:
             return 1
 
 if __name__ == "__main__":
-    game_state = GameState()
-    while True:
-        game_state.frame_step((random.randint(0, 2)))
+
+    def run_game():
+        game_state = GameState()
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+            game_state.frame_step((random.randint(0, 2)))
+    
+    run_game()
+    pygame.quit()

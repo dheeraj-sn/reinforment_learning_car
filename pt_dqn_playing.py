00a2c46cafea5589a827c368ceaffd97413f7ea3
Dheeraj Singh <dheeraj.student2@gmail.com>
2021-11-01 17:49:39 +0800

    Separated dqn from lstm

---
+++ pt_dqn_playing.py
@@ -0,0 +1,44 @@
+# import car_environment
+import newcarenv as car_environment
+import numpy as np
+from ptnn import neural_net
+import torch
+import pygame
+NUM_SENSORS = 3
+
+
+def play(model):
+
+    car_distance = 0
+    game_state = car_environment.GameState()
+
+    # Do nothing to get initial.
+    _, state = game_state.frame_step((2))
+    state = torch.from_numpy(state)
+    
+    model.eval()
+    
+    # Move.
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        car_distance += 1
+
+        # Choose action.
+        action = (torch.argmax(model(state.float())))
+
+        # Take action.
+        _, state = game_state.frame_step(action)
+        state = torch.from_numpy(state)
+
+        # Tell us something.
+        if car_distance % 1000 == 0:
+            print("Current distance: %d frames." % car_distance)
+
+
+if __name__ == "__main__":
+    saved_model = './saved-models/128-128-64-50000-10000.pt'
+    model = neural_net(NUM_SENSORS, [128, 128])
+    model.load_state_dict(torch.load(saved_model))
+    play(model)
---
+++ ptdqn.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+    
+class neural_net(nn.Module):
+    def __init__(self, num_sensors, params):
+        super(neural_net,self).__init__()
+
+        self.layer1 = nn.Linear(num_sensors, params[0])
+        self.layer2 = nn.Linear(params[0],params[1])
+        self.layer3 = nn.Linear(params[1],num_sensors)
+
+    def forward(self,x):
+        y = self.layer1(x)
+        y_hat = torch.relu(y)
+        y_hat = F.dropout(y_hat,0.2)
+        z = self.layer2(y_hat)
+        z_hat = torch.relu(z)
+        z_hat = F.dropout(z_hat, 0.2)
+        w = self.layer3(z_hat)
+        return w
